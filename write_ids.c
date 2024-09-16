6af6f0b0d40463df4992974f05825aae56a5b027
Jose <gomezjosec14@gmail.com>
2024-09-16 15:53:27 -0400

    added git id and psu id

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "jmg7930";
+    char STUDENT_ID[] = "930775051";
 
     return 0 ;
 }
